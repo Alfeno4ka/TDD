a62d6001ee80e0baaf676532d9b43f85dd51a679
Alena Sazhinova <alfeno4ka@gmail.com>
2024-11-02 16:07:27 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    private Map<String, String> contacts;
+
+    public PhoneBook() {
+        contacts = new TreeMap<>();
+    }
+
+    public int add(String name, String phone) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,31 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void testAddNewContact() {
+        int result = phoneBook.add("Александр", "+7 (990) 123-45-67");
+        assertEquals(1, result);
+
+        result = phoneBook.add("Алина", "+7 (995) 111-45-77");
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testAddDuplicateContact() {
+        phoneBook.add("Александр", "+7 (990) 123-45-67");
+        int result = phoneBook.add("Александр", "+7 (990) 123-45-67");
+        assertEquals(1, result);
+    }
 }
